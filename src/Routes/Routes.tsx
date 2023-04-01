5328119a3633f2e36f92107b9a583fc952a4cb8b
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-01 12:12:13 +0100

    update

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,6 +1,7 @@
 import React, { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import { HomeLayout } from "../components";
+import Gamezone from "../pages/Landingpage/Gamezone";
 
 export const Elements = createBrowserRouter([
   
@@ -9,7 +10,8 @@ export const Elements = createBrowserRouter([
     element: <HomeLayout />,
     children: [
       {
-        
+        path: "/solution",
+        element: <Gamezone/>,
       },
     ],
   },
