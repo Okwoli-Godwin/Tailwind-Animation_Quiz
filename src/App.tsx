6f4617fdfa3c3cfcdcc9d53a940b06d0ce8cca23
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-23 14:40:04 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Animation2 from './Animation2'
 import Animation1 from './Animation1'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Animation1 />
+      <Animation2 />
     </>
   )
 }
