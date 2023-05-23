5e0db328a390f0118132f6ae73bcf200f108c3f5
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-23 18:00:20 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Animation2 from './Animation2'
 import Animation1 from './Animation1'
+import Videobackground from './Videobackground'
 
 function App() {
 
   return (
-    <>
+    <div className='w-full'>
       <Animation1 />
       <Animation2 />
-    </>
+      <Videobackground />
+    </div>
   )
 }
 
---
+++ src/Videobackground.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import myVideo from "./assets/millionaire.mp4"
+
+const Videobackground = () => {
+    
+  return (
+      <div className='w-full h-screen'>
+          <video className='w-full h-screen object-cover absolute inset-0' src={myVideo} autoPlay={true} muted playsInline loop={true}></video>
+    </div>
+  )
+}
+
+export default Videobackground
