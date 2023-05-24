b4d836d76be50c8f1d85497e90ea1d6135b4788b
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-25 00:13:59 +0100

    update

--- src/Videobackground.tsx
+++ src/Videobackground.tsx
@@ -5,7 +5,13 @@ const Videobackground = () => {
     
   return (
       <div className='w-full h-screen'>
-          <video className='w-full h-screen object-cover absolute inset-0' src={myVideo} autoPlay={true} muted playsInline loop={true}></video>
+          <video className='w-full h-screen object-cover absolute inset-0'
+              src={myVideo}
+              autoPlay={true}
+              muted
+              playsInline
+              loop={true}>
+            </video>
     </div>
   )
 }
