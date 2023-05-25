e81cd980c95af2bee7166c174ebd04cde7470433
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-25 08:26:17 +0100

    update

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Hero = () => {
   return (
-      <div className='w-full h-screen bg-hero'>
+      <div className='w-full h-screen bg-hero flex justify-center items-center'>
+          <div className='w-[390px] h-[490px] bg-[rgba(0,0,0,0.5)] rounded p-5'>
+              <div className='text-white'>
+                  Setup Game
+              </div>
+          </div>
     </div>
   )
 }
