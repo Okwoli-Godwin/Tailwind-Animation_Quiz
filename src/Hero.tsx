4bd8eeafcf961cc20dd5e80fd079638dabf93593
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-25 11:38:51 +0100

    update

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -3,10 +3,8 @@ import React from 'react'
 const Hero = () => {
   return (
       <div className='w-full h-screen bg-hero flex justify-center items-center'>
-          <div className='w-[390px] h-[490px] bg-[rgba(0,0,0,0.5)] rounded p-5'>
-              <div className='text-white'>
-                  Setup Game
-              </div>
+          <div className='w-[390px] h-[490px] bg-[rgba(0,0,0,0.5)] rounded p-5 flex-col'>
+              <h1 className='text-white text-2xl'>Setup Game</h1>
           </div>
     </div>
   )
