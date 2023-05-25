9ca5c34f3e3fc70ffda574cddc200544d2ce2f91
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-25 13:47:49 +0100

    update

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,8 +1,12 @@
-
+import sound from "./assets/sound.mp3"
 
 const Hero = () => {
+
   return (
       <div className='w-full h-screen bg-hero flex justify-center items-center'>
+          <audio autoPlay loop>
+              <source src={ sound} type=""/>
+          </audio>
           <div className='w-[390px] h-[490px] bg-[rgba(0,0,0,0.5)] rounded p-5 flex-col'>
               <h1 className='text-white text-2xl'>Setup Game</h1>
 
